5d5346f65dbe37a1638061cc6f56fca9901376b6
dee-are-here <david.ward.uno@gmail.com>
2026-04-20 06:51:01 -0500

    updated list file

--- lists.py
+++ lists.py
@@ -15,7 +15,22 @@ import random
 # random_friend = random.choice(friends)
 # print(random_friend)
 
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen)
+
 fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
 vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
 dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
+
+# print(dirty_dozen)
+
+# print(dirty_dozen[0])
+# print(dirty_dozen[1])
+
+# print(dirty_dozen[1][2])
+# print(dirty_dozen[1][3])
+ 
+# print(dirty_dozen[1][1])
