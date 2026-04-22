9efe6fe281c9d2c42d98d89b20c51619718b26e3
dee-are-here <david.ward.uno@gmail.com>
2026-04-22 08:48:21 -0500

    function python file

---
+++ functions.py
@@ -0,0 +1,40 @@
+
+
+# print("Hi there!")
+# num_char = len("Hi there!")
+# print(num_char)
+
+# def my_function():
+#     print("Hello")
+#     print("Goodbye")
+#
+#
+# my_function()
+
+# # This is a function for moving a robot
+#
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# for step in range(6):
+#     jump()
+
+
+# This is my program
+print("This will run")
